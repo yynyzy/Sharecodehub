4481ab394c1766be1b5539ad54ddebbe4cf5af39
yanyinuo <1601530253@qq.com>
2021-11-09 22:21:54 +0800

    21-11-9

---
+++ src/middleware/file.middleware.js
@@ -0,0 +1,10 @@
+const Multer = require('koa-multer')
+
+const avatarUpload = Multer({
+    dest: './uploads/avatar'
+})
+const avatarHandler = avatarUpload.single('avatar')
+
+module.exports = {
+    avatarHandler
+}
--- src/route/file.router.js
+++ src/route/file.router.js
@@ -1,13 +1,11 @@
 const Router = require('koa-router')
-const Multer = require('koa-multer')
 
-const vertifyAuth = require('../middleware/auth.middleware')
+const { vertifyAuth } = require('../middleware/auth.middleware')
+const { avatarHandler } = require('../middleware/file.middleware')
+
+
 
 const fileRouter = new Router({ prefix: '/upload' })
-const avatarUpload = Multer({
-    dest: './uploads/avatar'
-})
-const avatarHandler = avatarUpload.single('avatar')
 
 fileRouter.post('/avatar ', vertifyAuth, avatarHandler)
 
